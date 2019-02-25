7fdd55db295210b64504e085779ca893f64bc34f
Stoyan Shukerov <stoichik@gmail.com>
2019-02-24 18:42:38 -0800

    Playing around with graphing the data.
    Going to be reworking everything this is just a save point.

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 const endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-features = [];
 
-// fetch JSON data
-fetch(endpoint)
-   .then(response => {
-      if (response.status === 200) {
-         response.json().then(data => features.push(...data.features));
-      } else {
-         textArea.innerHTML = "Whoops something went wrong with the API endpoint";
-         throw new Error("Could not fetch all_week.geojson");
+function getEarthquakes(callback) {
+   const features = [];
+   fetch(endpoint).then(response => {
+         if (response.status === 200) {
+            response.json().then(data => { 
+               features.push(...data.features);
+               callback(features);});
+         } else {
+            textArea.innerHTML = "Whoops something went wrong with the API endpoint";
+            throw new Error("Could not fetch all_week.geojson");
+         }
+   });
+   // return callback(features);
+}
+
+// finds all states in features and returns an object containing magnitudes of earthquakes
+function findMags(features) {
+   console.log(features);
+   const stateReg = /, ([\w]+)/g;
+   const magnitudes = features.reduce((res, item) => {
+      const mag = Math.round(item.properties.mag);
+      if (mag) {
+         if (!res[mag]) {
+            res[mag] = 0;
+         } 
+         res[mag]++;
       }
-})
+      return res;
+   }, {});
+   return magnitudes;
+}
 
+   // const states = features.reduce((obj, item) => {
+   // // stringMatch = /, ([\w]+)/.exec(item.properties.place.match(stateReg));
+   // stringMatch = item.properties.place.match(stateReg);
+   // // console.log(stringMatch);
+   // if (stringMatch) {
+   //    console.log(stringMatch);
+   //    //    if (!obj[state]) {
+   //    //       obj[state] = 0;
+   //    //    }
+   //    //    obj[state]++;
+   // }
+   // }, {});
 // finds matches in the place attribute of the JSON features
 // returns features that match the wordToMatch string
 function findMatches(regMatch, features) {
@@ -56,40 +88,46 @@ outputBtn.addEventListener('click', generateOutput);
 downloadBtn.addEventListener('click', downLoadOutput);
 
 // chart experimentation
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-       datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-       }]
-    },
-    options: {
-       scales: {
-          yAxes: [{
-             ticks: {
-                beginAtZero:true
-             }
-          }]
-        }
-    }
-});
+function graphMags(features) {
+   var ctx = document.getElementById("myChart");
+   var magnitudes = findMags(features);
+
+   var myChart = new Chart(ctx, {
+       type: 'bar',
+       data: {
+          labels: Object.keys(magnitudes),
+             datasets: [{
+                label: '# of earthquakes by magnitude',
+                data: Object.values(magnitudes),
+                backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   'rgba(75, 192, 192, 0.2)',
+                   'rgba(153, 102, 255, 0.2)',
+                   'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                   'rgba(255,99,132,1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+             }]
+       },
+       options: {
+          scales: {
+             yAxes: [{
+                ticks: {
+                   beginAtZero:true
+                }
+             }]
+           }
+       }
+   });
+}
+
+getEarthquakes(graphMags);
